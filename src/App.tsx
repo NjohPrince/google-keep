4bd1385c6e0459a137b77d3fa55db931633250a2
NjohPrince <jufredprince@gmail.com>
2024-01-24 16:41:42 +0100

    feat(icons): added material icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
+import AccessAlarmsOutlinedIcon from "@mui/icons-material/AccessAlarmsOutlined";
+
 const App = () => {
-  return <div>App</div>;
+  return <div>
+    <h1>Hello World</h1>
+    <AccessAlarmsOutlinedIcon />
+  </div>;
 };
 
 export default App;
